ad07c0773cb956b30e076cc564e7013690de4c33
rfrancois6 <romain57890@orange.fr>
2021-04-08 10:51:09 +0200

    modal

--- front/src/Themes/ImageTheme.tsx
+++ front/src/Themes/ImageTheme.tsx
@@ -1,36 +1,88 @@
 import "antd/dist/antd.css";
-import { useState } from "react";
-import { Modal, Button } from 'antd';
+import React, { useState } from "react";
+import { Button, Col, Modal, Row, Slider, Table } from 'antd';
+
+const columns = [
+  {
+    dataIndex: 'header_image',
+    key: 'header_image',
+    render: (record: any) => {
+      return (
+        <div>
+          <img style={{ width: '250px' }} src={record} />
+        </div>
+      );
+    },
+  }
+
+];
+
+function TextTheme() {
+  const apiUrl = 'http://steamback/api/getfirstgames?pageid=1';
+  const [game, setgame] = useState([]);
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then(data => setgame(data));
 
 
-function ImageTheme() {
+
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const showModal = () => {
     setIsModalVisible(true);
+    console.log(game)
   };
 
   const handleOk = () => {
     setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+  }
 
   return (
-<>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-    </>
+    <>
 
+      <a onClick={showModal}>
+        <Table dataSource={game} columns={columns} />
+      </a>
+
+      <Modal
+        visible={isModalVisible}
+        title="Title"
+        onOk={handleOk}
+        footer={[
+          <Button key="back" onClick={handleOk}>
+            Ok
+          </Button>
+        ]}
+      >
+
+      </Modal>
+
+      <Row gutter={[16, 24]}>
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+      </Row>
+      <Row gutter={[16, 24]}>
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+        <Col span={4} />
+      </Row>
+    </>
   );
 }
 
-export default ImageTheme;
+export default TextTheme;
--- front/src/Themes/TextTheme.tsx
+++ front/src/Themes/TextTheme.tsx
@@ -1,75 +1,12 @@
 import "antd/dist/antd.css";
-import React, {useState} from "react";
-import {Button, Modal, Table} from 'antd';
+import React, { useEffect, useState } from "react";
+import { Button, Carousel, Col, Modal, Row, Table } from 'antd';
+import axios from "axios";
+import "./TextTheme.css";
+import { ColumnsType } from "antd/lib/table";
 
-
-const dataSource = [
-    {
-
-        "appid": "365460",
-        "name": "CHARIOT WARS",
-        "release_date": "2015-05-25",
-        "english": "1",
-        "developer": "OM ENTERTAINMENT",
-        "publisher": "OM ENTERTAINMENT",
-        "platforms": "windows",
-        "required_age": "0",
-        "categories": "Single-player;Multi-player;Steam Achievements;Steam Cloud;Stats;Steam Leaderboards",
-        "genres": "Racing",
-        "steamspy_tags": "Racing;Arcade;Action",
-        "achievements": "16",
-        "positive_ratings": "1",
-        "negative_ratings": "9",
-        "average_playtime": "0",
-        "median_playtime": "0",
-        "owners": "0-20000",
-        "price": "4.99"
-
-    },
-    {
-        "appid": "371520",
-        "name": "Bounty Train",
-        "release_date": "2017-05-16",
-        "english": "1",
-        "developer": "Corbie Games",
-        "publisher": "Daedalic Entertainment",
-        "platforms": "windows;mac",
-        "required_age": "0",
-        "categories": "Single-player;Steam Achievements;Steam Trading Cards",
-        "genres": "Indie;Simulation;Strategy",
-        "steamspy_tags": "Simulation;Strategy;Indie",
-        "achievements": "30",
-        "positive_ratings": "572",
-        "negative_ratings": "240",
-        "average_playtime": "217",
-        "median_playtime": "217",
-        "owners": "50000-100000",
-        "price": "18.99"
-    },
-    {
-        "appid": "371550",
-        "name": "A Bastard's Tale",
-        "release_date": "2015-05-26",
-        "english": "1",
-        "developer": "No Pest Productions",
-        "publisher": "No Pest Productions",
-        "platforms": "windows",
-        "required_age": "0",
-        "categories": "Single-player;Steam Achievements;Full controller support;Steam Trading Cards;Steam Cloud",
-        "genres": "Action;Adventure;Indie",
-        "steamspy_tags": "Indie;Action;Adventure",
-        "achievements": "10",
-        "positive_ratings": "164",
-        "negative_ratings": "38",
-        "average_playtime": "408",
-        "median_playtime": "408",
-        "owners": "20000-50000",
-        "price": "3.99"
-    }
-];
 
 const columns = [
-
     {
         title: 'Name',
         dataIndex: 'name',
@@ -92,7 +29,7 @@ const columns = [
     },
     {
         title: 'Release Date',
-        dataIndex: 'release_date',
+        dataIndex: 'release_date_string',
         key: 'release_date',
     },
 
@@ -103,9 +40,10 @@ const columns = [
         render: (record: any) => {
             return (
                 <div>
-                    <img style={{width: '250px'}} src={record}/>
+                    <img style={{ width: '250px' }} src={record} />
                 </div>
-            );},
+            );
+        },
     },
     {
         title: 'Required Age',
@@ -115,50 +53,142 @@ const columns = [
 ];
 
 
-  function TextTheme() {
-    const apiUrl = 'http://steamback/api/getfirstgames?pageid=1';
-    const [game, setgame] = useState([]);
+interface Game {
+    appid: 'appid'
+    name: 'name'
+    genres: 'genres'
+    publisher: 'publisher'
+    developer: 'developer'
+    release_date: 'release_date'
+    image: 'image'
+    required_age: 'required_age'
+}
 
-     fetch(apiUrl)
-        .then((response) => response.json())
-        .then(data => setgame(data));
+function TextTheme() {
+    const [games, setGames] = useState<Game[]>([]);
+    const [isLoading, setIsLoading] = useState(true)
 
 
+    useEffect(() => {
+        axios.get<Game[]>('http://steamback/api/getfirstgames?pageid=1')
+            .then(({ data }) => {
+                setGames(data)
+            })
+            .catch((error) => console.error())
+            .finally(() => setIsLoading(false))
+    }, [])
 
     const [isModalVisible, setIsModalVisible] = useState(false);
-
     const showModal = () => {
         setIsModalVisible(true);
     };
 
     const handleOk = () => {
         setIsModalVisible(false);
-    };
+    }
 
+    console.log(games)
     return (
-
         <>
 
-            <a onClick={showModal}>
-                <Table dataSource={game} columns={columns}/>
-            </a>
+            <Table dataSource={games} columns={columns} />
+            <div>
+                <ul>
+                    {games.data.map((game: Game) => (
+                        <li key={game.appid}>{game.name} - {game.name}</li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* <Button onClick={showModal}>Modal</Button> */}
 
             <Modal
                 visible={isModalVisible}
-                title="Title"
+                width="1100px"
+                title="Modal de #Nomdujeu"
                 onOk={handleOk}
                 footer={[
-                    <Button key="back" onClick={handleOk}>
-                        Ok
-                    </Button>,
+                    // <Button key="back" onClick={handleOk}>
+                    //     Ok
+                    // </Button>
                 ]}
             >
-                <p>aa</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                <>
+                    <Row>
+                        <Col span={8} offset={8}>
+                            <h1 className="center">Nom du jeu</h1>
+                        </Col>
+                        <Col span={4} offset={4}>
+                            <h1 className="note">Note : 99</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12} offset={6}>
+                            <Carousel
+                                autoplay
+                            >
+                                <div className="carousel">
+                                    <img src="https://lh3.googleusercontent.com/proxy/4B3IORMMoc_1ycCUsq8FyzznkE_AFvtN3tdhUEk-NIx-aZJIqp7OuxFVHSBf0u_cITo0vzSzgjXgMRSiX5qaflYP0Q1fovtlM8l2rwb8OcrOeTVn9vL4csw_stMvCeV3-bA9TcfyO5op_qu5SkxOiZ-eJ-b5eu_5yvEEJy4dFYLbtoCThrVDiPVqrJMfUkhm1qYo2JeAkkEP2G9nl1KuoW9vP3Qawsx-_dpJ8M4HD_C0f0C_-oo1rA" />
+                                </div>
+                                <div className="carousel">
+                                    <img src="https://store-images.s-microsoft.com/image/apps.34721.14497007681830249.1b307a7b-fe75-442d-9bb3-13b865e22c88.02d5bc74-b5bd-444b-9e83-be774d5f4fd5?mode=scale&q=90&h=1080&w=1920&format=jpg" />
+                                </div>
+                                <div className="carousel">
+                                    <img src="https://www.gamereactor.fr/media/99/deadcells_2539923b.jpg" />
+                                </div>
+                            </Carousel>
+                        </Col>
+                        {/* <Col span={12} offset={2}>
+                            <img className="temp" src="https://lh3.googleusercontent.com/proxy/4B3IORMMoc_1ycCUsq8FyzznkE_AFvtN3tdhUEk-NIx-aZJIqp7OuxFVHSBf0u_cITo0vzSzgjXgMRSiX5qaflYP0Q1fovtlM8l2rwb8OcrOeTVn9vL4csw_stMvCeV3-bA9TcfyO5op_qu5SkxOiZ-eJ-b5eu_5yvEEJy4dFYLbtoCThrVDiPVqrJMfUkhm1qYo2JeAkkEP2G9nl1KuoW9vP3Qawsx-_dpJ8M4HD_C0f0C_-oo1rA" />
+                        </Col> */}
+                    </Row>
+
+                    <Row className="about">
+                        <Col span={12} offset={6}>
+                            <h1>About this game</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ea ab laudantium, neque dolore repellat dicta ut repellendus voluptate eos, magni explicabo laborum unde temporibus perferendis libero mollitia accusamus. Porro?
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={20}>
+                            <h2>Features</h2>
+                            <ul>
+                                <li>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum placeat laboriosam totam perspiciatis asperiores vitae architecto illo optio! Rem, beatae aliquid eligendi assumenda dolorem accusantium non inventore praesentium laudantium accusamus!
+                            </li>
+                                <li>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto id, quidem, laboriosam rerum magnam saepe error optio veritatis soluta, voluptatum ipsam ex esse.
+                            </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={16}>
+                            <h2>Config</h2>
+                            <ul>
+                                <li>
+                                    Processeur : Intel Core i5 ou AMD Ryzen 5
+                                </li>
+                                <li>
+                                    Mémoire Vive RAM : 16 Go de RAM DDR4 2133 hz
+                                </li>
+                                <li>
+                                    Carte graphique : Nvidia GeForce GTX 1060 6 Go
+                                </li>
+                                <li>
+                                    Disque dur : Disque SSD de 250 Go en complément d’un disque dur HDD de 1 To
+                                </li>
+                                <li>
+                                    Système d’exploitation : Windows 10 (64 bit)
+                                </li>
+
+                            </ul>
+                        </Col>
+                    </Row>
+                </>
+
             </Modal>
         </>
     );
