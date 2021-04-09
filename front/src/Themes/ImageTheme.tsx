765c39a95a7f2f3c3d21840c54dd4f2549396115
EldennOff <user8@example.com>
2021-04-09 10:55:58 +0200

    Modif front

--- front/src/Themes/ImageTheme.tsx
+++ front/src/Themes/ImageTheme.tsx
@@ -1,6 +1,6 @@
 import "antd/dist/antd.css";
 import React, {useEffect, useState} from "react";
-import {Button, Col, Collapse, Input, Modal, Pagination, Radio, Row, Select} from 'antd';
+import {Button, Carousel, Col, Collapse, Input, Modal, Pagination, Radio, Row, Select} from 'antd';
 import axios from "axios";
 
 interface Game {
@@ -27,6 +27,50 @@ interface Temp {
     total_games: string
     total_pages: string
     data: Game[]
+}
+
+interface Info {
+    steam: Game
+    description: Description
+    media: Media
+    requirements: Requirements
+    // support:
+    // tags:
+
+}
+
+interface SteamCall {
+    total_games: string
+    total_pages: string
+    data: Game[]
+}
+
+interface Description {
+    appid: string
+    detailed_description: Object
+    about_the_game: Object
+    short_description: Object
+}
+
+interface Media {
+    appid: string
+    header_image: string
+    screenshots: string
+    background: string
+    movies: string
+}
+
+interface Requirements {
+    minimum: string
+    recommanded: string
+
+}
+
+interface Support {
+
+}
+interface Tags {
+
 }
 
 
@@ -61,6 +105,8 @@ function ImageTheme() {
         setRadioButton(event.target.value);
     }
 
+    const [gameCall, setGameCall] = useState<Info | null>(null);
+
     const [input_name, setInput_name] = useState<string>("");
     const [input_categories, setInput_categories] = useState<string>("");
     const [input_genres, setInput_genres] = useState<string>("");
@@ -76,7 +122,15 @@ function ImageTheme() {
     }
 
     const [apiCall, setApiCall] = useState<Temp | null>(null);
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getGameByAppId = (appid: string) => {
+        axios.get<Info>('http://steamback/api/game?appi=' + appid)
+            .then(({ data }) => {
+                setGameCall(data);
+            })
+            .catch((error) => console.error())
+    }
 
     useEffect(() => {
         axios.get<Temp>("http://steamback/api/getfirstgames?pageid=1")
@@ -107,23 +161,23 @@ function ImageTheme() {
     function researchData(pageid: number) {
         console.log(input_name)
 
-const bodyFormData = new FormData();
+        const bodyFormData = new FormData();
 
-        bodyFormData.append('name',input_name);
-        bodyFormData.append('categories',input_categories);
-        bodyFormData.append('genres',input_genres);
-        bodyFormData.append('developer',input_developer);
-        bodyFormData.append('publisher',input_publisher);
-        bodyFormData.append('asc_desc',radioButton);
-        bodyFormData.append('sortby',select_shortBy);
-        bodyFormData.append("pageid",pageid.toString());
+        bodyFormData.append('name', input_name);
+        bodyFormData.append('categories', input_categories);
+        bodyFormData.append('genres', input_genres);
+        bodyFormData.append('developer', input_developer);
+        bodyFormData.append('publisher', input_publisher);
+        bodyFormData.append('asc_desc', radioButton);
+        bodyFormData.append('sortby', select_shortBy);
+        bodyFormData.append("pageid", pageid.toString());
         axios({
             method: "post",
             url: "http://steamback/api/searchgame",
             data: bodyFormData,
-            headers: { "Content-Type": "multipart/form-data" },
+            headers: {"Content-Type": "multipart/form-data"},
         }).then(({data}) => {
-           setApiCall(data);
+            setApiCall(data);
         })
             .catch((error) => console.error())
             .finally(() => setIsLoading(false))
@@ -139,28 +193,34 @@ const bodyFormData = new FormData();
                 <Collapse defaultActiveKey={['0']} onChange={callback}>
                     <Panel header="Recherche avancée" key="1">
                         Name : <Input id="input_name" value={input_name} type="text" onChange={handleName}/><br/><br/>
-                        Categories : <Input id="input_categories" type="text" value={input_categories} onChange={handleCategories}/><br/><br/>
-                        Genres : <Input id="input_genres" type="text"  value={input_genres} onChange={handleGenres}/><br/><br/>
-                        Developer : <Input id="input_developer" type="text"  value={input_developer} onChange={handleDeveloper}/><br/><br/>
-                        Publisher : <Input id="input_publisher" type="text"  value={input_publisher} onChange={handlePublisher}/> <br/><br/>
-<div>
-                        Sort By : <Select defaultValue="name" id="select_sortBy" style={{width: "120px"}} >
-                        <option value="name" onChange={handleShortBy}>Name</option>
-                        <option value="release_date" onChange={handleShortBy}>Release Date</option>
-                        <option value="developer" onChange={handleShortBy}>Developer</option>
-                        <option value="publisher" onChange={handleShortBy}>Publisher</option>
-                        <option value="required_age" onChange={handleShortBy}>Required Age</option>
-                        <option value="percentage_ratings" >Percentage Ratings</option>
-                    </Select>
-</div>
+                        Categories : <Input id="input_categories" type="text" value={input_categories}
+                                            onChange={handleCategories}/><br/><br/>
+                        Genres : <Input id="input_genres" type="text" value={input_genres}
+                                        onChange={handleGenres}/><br/><br/>
+                        Developer : <Input id="input_developer" type="text" value={input_developer}
+                                           onChange={handleDeveloper}/><br/><br/>
+                        Publisher : <Input id="input_publisher" type="text" value={input_publisher}
+                                           onChange={handlePublisher}/> <br/><br/>
+                        <div>
+                            Sort By : <Select defaultValue="name" id="select_sortBy" style={{width: "120px"}}>
+                            <option value="name" onChange={handleShortBy}>Name</option>
+                            <option value="release_date" onChange={handleShortBy}>Release Date</option>
+                            <option value="developer" onChange={handleShortBy}>Developer</option>
+                            <option value="publisher" onChange={handleShortBy}>Publisher</option>
+                            <option value="required_age" onChange={handleShortBy}>Required Age</option>
+                            <option value="percentage_ratings">Percentage Ratings</option>
+                        </Select>
+                        </div>
                         <div style={{textAlign: "justify"}}>
-                            <Radio.Group  defaultValue="desc">
+                            <Radio.Group defaultValue="desc">
                                 <Radio value="desc" onChange={handleRadio}>Descendant</Radio>
-                                <Radio value="asc" onChange={handleRadio} >Ascendant</Radio>
+                                <Radio value="asc" onChange={handleRadio}>Ascendant</Radio>
                             </Radio.Group>
                         </div>
                         <div>
-                            <Button value="Submit" id="submit" onClick={()=>{researchData(1)}}>Research</Button>
+                            <Button value="Submit" id="submit" onClick={() => {
+                                researchData(1)
+                            }}>Research</Button>
                         </div>
                     </Panel>
                 </Collapse>
@@ -170,22 +230,96 @@ const bodyFormData = new FormData();
             <div style={{textAlign: "center"}}>
 
                 {!isLoading && apiCall ? apiCall.data.map((game: Game) => (
-                    <img key={game.appid} style={{margin: "4px 4px 4px 4px"}} src={game.header_image}></img>
+                    <img key={game.appid} style={{margin: "4px 4px 4px 4px"}} src={game.header_image} onClick={ ()=> {
+                        getGameByAppId(game.appid.toString())
+                        showModal()
+                    }}/>
                 )) : <p>Loading</p>}
 
             </div>
-            <Modal
-                visible={isModalVisible}
-                title="Title"
-                onOk={handleOk}
-                footer={[
-                    <Button key="back" onClick={handleOk}>
-                        Ok
-                    </Button>
-                ]}
-            >
+            {
+                gameCall && (
 
-            </Modal>
+                    <Modal
+                        visible={isModalVisible}
+                        width="1100px"
+                        title={gameCall.steam.name}
+                        onOk={handleOk}
+                        footer={[
+                            <Button key="back" onClick={handleOk}>
+                                Ok
+                            </Button>
+                        ]}
+                    >
+                        <>
+                            <Row>
+                                <Col span={8} offset={8}>
+                                    <h1 className="center">{gameCall.steam.name}</h1>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={12} offset={6}>
+                                    <Carousel
+                                        autoplay
+                                    >
+                                        <div className="carousel">
+                                            <img
+                                                src={gameCall.media.screenshots[0]} />
+                                        </div>
+                                        <div className="carousel">
+                                            <img
+                                                src={gameCall.media.screenshots[1]} />
+                                        </div>
+                                        <div className="carousel">
+                                            <img
+                                                src={gameCall.media.screenshots[2]} />
+                                        </div>
+                                    </Carousel>
+                                </Col>
+                                {/* <Col span={12} offset={2}>
+                            <img className="SteamCall" src="https://lh3.googleusercontent.com/proxy/4B3IORMMoc_1ycCUsq8FyzznkE_AFvtN3tdhUEk-NIx-aZJIqp7OuxFVHSBf0u_cITo0vzSzgjXgMRSiX5qaflYP0Q1fovtlM8l2rwb8OcrOeTVn9vL4csw_stMvCeV3-bA9TcfyO5op_qu5SkxOiZ-eJ-b5eu_5yvEEJy4dFYLbtoCThrVDiPVqrJMfUkhm1qYo2JeAkkEP2G9nl1KuoW9vP3Qawsx-_dpJ8M4HD_C0f0C_-oo1rA" />
+                        </Col> */}
+                            </Row>
+
+                            <Row className="about">
+                                <Col span={20} offset={2}>
+                                    <h1>About this game</h1>
+                                    <p>
+                                        {gameCall.description.short_description}
+                                    </p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={20}>
+                                    <h2>Features</h2>
+                                    <ul>
+                                        <li>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum placeat laboriosam
+                                            totam perspiciatis asperiores vitae architecto illo optio! Rem, beatae aliquid
+                                            eligendi assumenda dolorem accusantium non inventore praesentium laudantium
+                                            accusamus!
+                                        </li>
+                                        <li>
+                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto id, quidem,
+                                            laboriosam rerum magnam saepe error optio veritatis soluta, voluptatum ipsam ex
+                                            esse.
+                                        </li>
+                                    </ul>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={16}>
+                                    <h2>Requirements :</h2>
+                                    <ul>
+                                        <li>{gameCall.requirements.minimum}</li>
+                                    </ul>
+                                </Col>
+                            </Row>
+                        </>
+
+                    </Modal>
+                )
+            }
 
             <Row gutter={[16, 24]}>
                 <Col span={4}/>
@@ -211,7 +345,9 @@ const bodyFormData = new FormData();
                 <Col span={4}/>
             </Row>
             <Pagination showSizeChanger={false} pageSize={20} defaultCurrent={1} total={total_games}
-                        onChange={(page) =>{researchData(page) }}/>
+                        onChange={(page) => {
+                            researchData(page)
+                        }}/>
 
         </>
     );
--- front/src/Themes/TextTheme.tsx
+++ front/src/Themes/TextTheme.tsx
@@ -1,6 +1,6 @@
 import "antd/dist/antd.css";
 import React, { useEffect, useState } from "react";
-import { Button, Carousel, Col, Collapse, Modal, Row, Table } from 'antd';
+import {Button, Carousel, Col, Collapse, Input, Modal, Pagination, Radio, Row, Select, Table} from 'antd';
 import axios from "axios";
 import "./TextTheme.css";
 
@@ -120,9 +120,52 @@ interface Tags {
 
 
 function TextTheme() {
+
+    const handleName = (event: any) => {
+        setInput_name(event.target.value);
+    }
+
+    const handleCategories = (event: any) => {
+        setInput_categories(event.target.value);
+    }
+
+    const handleGenres = (event: any) => {
+        setInput_genres(event.target.value);
+    }
+
+    const handleDeveloper = (event: any) => {
+        setInput_developer(event.target.value);
+    }
+
+    const handlePublisher = (event: any) => {
+        setInput_publisher(event.target.value);
+    }
+
+    const handleShortBy = (event: any) => {
+        setSelect_shortBy(event.target.value);
+    }
+
+    const handleRadio = (event: any) => {
+        setRadioButton(event.target.value);
+    }
+
     const [apiCall, setApiCall] = useState<SteamCall | null>(null);
-    const [gameCall, setGameCall] = useState<Info | null>(null)
-    const [isLoading, setIsLoading] = useState(true)
+    const [gameCall, setGameCall] = useState<Info | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [input_name, setInput_name] = useState<string>("");
+    const [input_categories, setInput_categories] = useState<string>("");
+    const [input_genres, setInput_genres] = useState<string>("");
+    const [input_developer, setInput_developer] = useState<string>("");
+    const [input_publisher, setInput_publisher] = useState<string>("");
+    const [select_shortBy, setSelect_shortBy] = useState<string>("");
+    const [radioButton, setRadioButton] = useState<string>("");
+
+    const {Panel} = Collapse;
+
+    function callback(key: any) {
+        console.log(key);
+    }
 
 
     useEffect(() => {
@@ -152,10 +195,69 @@ function TextTheme() {
             .catch((error) => console.error())
     }
 
-    console.log(gameCall)
+    let total_games = Number(apiCall?.total_games)
+
+    console.log(gameCall);
+
+    function researchData(pageid: number) {
+        console.log(input_name)
+
+        const bodyFormData = new FormData();
+
+        bodyFormData.append('name',input_name);
+        bodyFormData.append('categories',input_categories);
+        bodyFormData.append('genres',input_genres);
+        bodyFormData.append('developer',input_developer);
+        bodyFormData.append('publisher',input_publisher);
+        bodyFormData.append('asc_desc',radioButton);
+        bodyFormData.append('sortby',select_shortBy);
+        bodyFormData.append("pageid",pageid.toString());
+        axios({
+            method: "post",
+            url: "http://steamback/api/searchgame",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+        }).then(({data}) => {
+            setApiCall(data);
+        })
+            .catch((error) => console.error())
+            .finally(() => setIsLoading(false))
+
+
+    }
 
     return (
         <>
+            <div style={{textAlign: "left"}}>
+                <Collapse defaultActiveKey={['0']} onChange={callback}>
+                    <Panel header="Recherche avancée" key="1">
+                        Name : <Input id="input_name" value={input_name} type="text" onChange={handleName}/><br/><br/>
+                        Categories : <Input id="input_categories" type="text" value={input_categories} onChange={handleCategories}/><br/><br/>
+                        Genres : <Input id="input_genres" type="text"  value={input_genres} onChange={handleGenres}/><br/><br/>
+                        Developer : <Input id="input_developer" type="text"  value={input_developer} onChange={handleDeveloper}/><br/><br/>
+                        Publisher : <Input id="input_publisher" type="text"  value={input_publisher} onChange={handlePublisher}/> <br/><br/>
+                        <div>
+                            Sort By : <Select defaultValue="name" id="select_sortBy" style={{width: "120px"}} >
+                            <option value="name" onChange={handleShortBy}>Name</option>
+                            <option value="release_date" onChange={handleShortBy}>Release Date</option>
+                            <option value="developer" onChange={handleShortBy}>Developer</option>
+                            <option value="publisher" onChange={handleShortBy}>Publisher</option>
+                            <option value="required_age" onChange={handleShortBy}>Required Age</option>
+                            <option value="percentage_ratings" >Percentage Ratings</option>
+                        </Select>
+                        </div>
+                        <div style={{textAlign: "justify"}}>
+                            <Radio.Group  defaultValue="desc">
+                                <Radio value="desc" onChange={handleRadio}>Descendant</Radio>
+                                <Radio value="asc" onChange={handleRadio} >Ascendant</Radio>
+                            </Radio.Group>
+                        </div>
+                        <div>
+                            <Button value="Submit" id="submit" onClick={()=>{researchData(1)}}>Research</Button>
+                        </div>
+                    </Panel>
+                </Collapse>
+            </div>
             <div>
                 {!isLoading && apiCall ?
                     <Table dataSource={apiCall.data} columns={columns} pagination={false} rowKey={record => record.appid} onRow={(record, rowIndex) => {
@@ -254,6 +356,8 @@ function TextTheme() {
                     </Modal>
                 )
             }
+            <Pagination showSizeChanger={false} pageSize={20} defaultCurrent={1} total={total_games}
+                        onChange={(page) =>{researchData(page) }}/>
         </>
     );
 }
